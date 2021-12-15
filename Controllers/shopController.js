41db6f34e3c2b2e2d2101af87f1ddcfdf5e4e030
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-12-15 12:18:58 -0500

    middleware part 2 in progress

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -1,8 +1,30 @@
+exports.middlewareSample = (req, res, next) => {
+  req.name = "TEST";
+  next();
+};
+
 exports.homepage = async (req, res) => {
   const brands = ["Gucci", "Armani", "Tommy Hilfiger", "Polo"];
 
   try {
     res.send(brands);
+    console.log(req.name);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.authMiddleware = (req, res, next) => {
+  if (req.body.user) {
+    next();
+  } else {
+    res.json("you must be signed in");
+  }
+};
+
+exports.authPage = async (req, res) => {
+  try {
+    res.json(req.body.user);
   } catch (error) {
     console.log(error);
   }
--- Routes/index.js
+++ Routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = new express.Router();
 const shopController = require("../Controllers/shopController");
 
-router.get("/", shopController.homepage);
-
+router.get("/", shopController.middlewareSample, shopController.homepage);
+router.get("/auth", shopController.authMiddleware, shopController.authPage);
 module.exports = router;
