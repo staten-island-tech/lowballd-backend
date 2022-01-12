435401be3fa282e05388a2a3d8693c3f98b082b0
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2022-01-12 12:29:53 -0500

    push

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
 
-mongoose.connect(`${process.env.DATABASE}`, {
-  useNewURLParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(`${process.env.DATABASE}`, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to DB"));
 
 mongoose.connection.on("error", (err) => {
   console.log(`${err.message}`);
