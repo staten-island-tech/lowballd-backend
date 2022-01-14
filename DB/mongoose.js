eadefb6e0a87c67a119c22181f702bda3294f6c6
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2022-01-14 12:24:05 -0500

    why isnt the console.log poppingup in my terminal

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
-
+console.log(process.env.DATABASE);
 mongoose
   .connect(`${process.env.DATABASE}`, {
     useNewURLParser: true,
