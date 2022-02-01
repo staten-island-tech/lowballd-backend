23d2e4e96874e41a75e609c990e94c1c3ac98e44
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-01-31 23:04:16 -0500

    starting video 9

--- Controllers/shopController.js
+++ Controllers/shopController.js
@@ -15,8 +15,17 @@ exports.createShop = async (req, res) => {
   try {
     const shop = new Shop(req.body);
     await shop.save();
-    // res.json(shop);
+    res.json(shop);
   } catch (error) {
     res.status(500).json(error);
+  }
+};
+
+exports.getShops = async (req, res) => {
+  try {
+    const Shops = await Shop.find().limit(3); //.limit is limiting how many thigns pop up upon request
+    res.json(Shops);
+  } catch (error) {
+    console.log(error);
   }
 };
