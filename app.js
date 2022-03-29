d9ee9001fbb9fa370b84f5bf9b2defb754b11e96
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-03-28 22:37:26 -0400

    token test works now

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express"); // this file requires express server
 const port = process.env.PORT || 3001; // use external server port OR local 3000
 const app = express(); //instantiate express
 const cors = require("cors");
-const authController = require("./Controllers/authController");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 require("./DB/mongoose"); //ensures mongoos connects and runs
 const posts = require("./Routes/api/post");
 app.use("/api/posts", posts);
@@ -14,21 +15,28 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-const { auth } = require("express-openid-connect");
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the signing keys provided by the JWKS endpoint
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-5t61kzw2.us.auth0.com/.well-known/jwks.json`,
+  }),
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: "http://localhost:3000",
-  clientID: "ZxpmPPmMWimzX2lIp5NmoTNkzb0Q3TTN",
-  issuerBaseURL: "https://dev-5t61kzw2.us.auth0.com",
-  secret: "SwzqN_DZpqNDahEKLRgC7GlKa0eZXL_7SYPRejZpLVCJLTQSHIuW2ezUpjxwMFot",
-};
+  // Validate the audience and the issuer
+  audience: "http://localhost:3001/api", //replace with your API's audience, available at Dashboard > APIs
+  issuer: "https://dev-5t61kzw2.us.auth0.com/",
+  algorithms: ["RS256"],
+});
 
-app.use(auth(config));
-app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? `${req.oidc.user.name}` : "Logged out");
-  console.log(req.oidc.user);
+app.get("/authorized", checkJwt, async function (req, res) {
+  try {
+    console.log(req.user);
+    res.json(req.user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port, () => {
