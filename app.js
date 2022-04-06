05cea7ba176d975a06d52d1f9e6afda3da715476
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2022-04-06 11:11:40 -0400

    not needed

--- app.js
+++ app.js
@@ -39,21 +39,6 @@ app.get("/authorized", checkJwt, async function (req, res) {
     console.log(error);
   }
 });
-const { auth, requiresAuth } = require("express-openid-connect");
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
-  secret: process.env.SECRET,
-};
-
-app.use(auth(config));
-app.get("/", (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? `${req.oidc.user}` : "Logged out");
-  console.log(req.oidc.user);
-});
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
