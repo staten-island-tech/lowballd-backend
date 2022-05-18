16e01406546aa3bb4643961e75fc8367910396cd
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2022-05-17 21:51:54 -0400

    just a rough draft, doesn't work yet

--- Routes/api/user.js
+++ Routes/api/user.js
@@ -51,7 +51,7 @@ const upload = multer({ storage: storage });
 
 router.get("/:id", userController.getUser);
 
-router.patch("/update/:id", checkJwt, async (req, res) => {
+router.patch("/update/:id", async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
     const updates = Object.keys(req.body);
@@ -61,6 +61,29 @@ router.patch("/update/:id", checkJwt, async (req, res) => {
     res.json(user);
   } catch (err) {
     res.status(400).send(err.message);
+  }
+});
+
+//This doesn't work yet because I need to figure out what needs to happen for a person to follow another person.
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { totalFollowers: req.body.userId } });
+        await currentUser.updateOne({
+          $push: { totalFollowing: req.params.id },
+        });
+        res.status(200).json("user has been followed");
+      } else {
+        res.status(403).json("you allready follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant follow yourself");
   }
 });
 
